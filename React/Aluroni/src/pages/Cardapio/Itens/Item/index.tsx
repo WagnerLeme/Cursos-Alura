17d8c42253f0edc4132792c913a7e00b69b56c41
Wagner Leme <82535594+WagnerLeme@users.noreply.github.com>
2023-05-05 17:07:45 -0300

    Refatoração do código e uso da biblioteca Router

--- React/Aluroni/src/pages/Cardapio/Itens/Item/index.tsx
+++ React/Aluroni/src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,35 +1,25 @@
 import styles from './Item.module.scss';
-import cardapio from '../itens.json';
-import classNames from 'classnames';
+import TagsPrato from 'components/TagsPrato';
+import { Prato } from 'types/Prato';
+import { useNavigate } from 'react-router-dom';
 
-type Props = typeof cardapio[0];
 
-export default function Item(props: Props) {
-    const { title, description, category, size, serving, price, photo } = props;
-    return (
-        <div className={styles.item}>
-            <div className={styles.item__imagem}>
-                <img src={photo} alt="{title}" />
-            </div>
+export default function Item(props: Prato) {
+  const navigate = useNavigate();
+  const { id, title, description, photo } = props;
+  return (
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt="{title}" />
+      </div>
 
-            <div className={styles.item__descricao}>
-                <div className={styles.item__titulo}>
-                    <h2> {title} </h2>
-                    <p> {description} </p>
-                </div>
-                <div className={styles.item__tags}>
-                    <div className={classNames({
-                        [styles.item__tipo]: true,
-                        [styles[`item__tipo__${category.label.toLowerCase()}`]]: true  
-                    })}
-                    > 
-                    {category.label}
-                    </div>
-                    <div className={styles.item__porcao}> {size}g </div>
-                    <div className={styles.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? "": "s"} </div>
-                    <div className={styles.item__valor}> R$ {price.toFixed(2)} </div>
-                </div>
-            </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+          <h2> {title} </h2>
+          <p> {description} </p>
         </div>
-    )
+        <TagsPrato {...props} />
+      </div>
+    </div>
+  );
 }
--- React/Aluroni/src/pages/Cardapio/Itens/index.tsx
+++ React/Aluroni/src/pages/Cardapio/Itens/index.tsx
@@ -1,7 +1,8 @@
-import cardapio from './itens.json'; 
+import cardapio from 'data/cardapio.json'; 
 import Item from './Item';
 import styles from './Itens.module.scss';
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { Cardapio } from 'types/Prato';
 
 interface Props {
   busca: string,
@@ -23,16 +24,16 @@ export default function Itens(props: Props) {
     return true;
   }
 
-  function ordenar(novaLista: typeof cardapio) {
+  function ordenar(novaLista: Cardapio) {
     switch(ordenador) {
-      case 'porcao': 
-        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
-      case 'qtd_pessoas':
-        return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
-      case 'preco':
-        return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
-      default:
-        return novaLista; 
+    case 'porcao': 
+      return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+    case 'qtd_pessoas':
+      return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
+    case 'preco':
+      return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
+    default:
+      return novaLista; 
     }
   }
   useEffect(() => {
@@ -46,5 +47,5 @@ export default function Itens(props: Props) {
         <Item key={item.id} {...item} />
       ))}
     </div>
-  )
+  );
 }
